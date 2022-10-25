a05ad837194181a4979f64165fdcd081985797ef
bhoon-coding <bhooncoding@gmail.com>
2022-10-25 11:22:44 +0900

    [프로그래머스] 인덱스 바꾸기

--- Programmers/Lv.0/인덱스 바꾸기/MyPlayground.playground/Contents.swift
+++ Programmers/Lv.0/인덱스 바꾸기/MyPlayground.playground/Contents.swift
@@ -1,16 +1,16 @@
 import Foundation
 
-func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
-    var resultString: [Character] = Array(my_string)
-    var tempArr: [Character] = []
-    tempArr.append(Array(my_string)[num1])
-    tempArr.append(Array(my_string)[num2])
-    resultString[num1] = tempArr.popLast()!
-    resultString[num2] = tempArr.popLast()!
-    return String(resultString)
-}
+//func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
+//    var resultString: [Character] = Array(my_string)
+//    var tempArr: [Character] = []
+//    tempArr.append(Array(my_string)[num1])
+//    tempArr.append(Array(my_string)[num2])
+//    resultString[num1] = tempArr.popLast()!
+//    resultString[num2] = tempArr.popLast()!
+//    return String(resultString)
+//}
 
-solution("hello", 1, 2)
+//solution("hello", 1, 2)
 
 // Refactoring
 
@@ -23,3 +23,11 @@ solution("hello", 1, 2)
 //}
 //
 //solution("hello", 1, 2)
+
+func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
+    var arr = Array(my_string)
+    arr.swapAt(num1, num2)
+    
+    return String(arr)
+}
+solution("hello", 1, 2)
