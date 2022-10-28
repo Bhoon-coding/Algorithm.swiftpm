a90f395c1ce1aaaee9abf7b9e964bfd725e7b14b
bhoon-coding <bhooncoding@gmail.com>
2022-10-28 11:03:52 +0900

    [프로그래머스] 최댓값 만들기(2) 전체통과

--- Programmers/Lv.0/최댓값 만들기(2)/MyPlayground.playground/Contents.swift
+++ Programmers/Lv.0/최댓값 만들기(2)/MyPlayground.playground/Contents.swift
@@ -1,15 +1,23 @@
 import Foundation
 
+//func solution(_ numbers:[Int]) -> Int {
+//    var maxValue: Int = 0
+//    for i in 0...numbers.count - 1 {
+//        for j in 0...numbers.count - 1 {
+//            if i != j {
+//                maxValue = max(maxValue, numbers[i] * numbers[j])
+//            }
+//        }
+//    }
+//    return maxValue
+//}
+
+import Foundation
+
 func solution(_ numbers:[Int]) -> Int {
-    var maxValue: Int = 0
-    for i in 0...numbers.count - 1 {
-        for j in 0...numbers.count - 1 {
-            if i != j {
-                maxValue = max(maxValue, numbers[i] * numbers[j])
-            }
-        }
-    }
-    return maxValue
+    var numbers = numbers.sorted()
+
+    return max(numbers[0] * numbers[1], numbers[numbers.count-1] * numbers[numbers.count - 2])
 }
 
-solution([-1, 0, 0])
+solution([10, -20, 30, 5, 5, -20, 5])
--- Programmers/Lv.0/최댓값 만들기(2)/README.md
+++ Programmers/Lv.0/최댓값 만들기(2)/README.md
@@ -6,6 +6,7 @@
 
 ### 풀이 <br>
 ```swift 
+// 테스트케이스 7번 미통과.. 
 func solution(_ numbers:[Int]) -> Int {
     var maxValue: Int = 0
     for i in 0...numbers.count - 1 {
@@ -17,10 +18,20 @@ func solution(_ numbers:[Int]) -> Int {
     }
     return maxValue
 }
+
+// 다른사람의 풀이 
+func solution(_ numbers:[Int]) -> Int {
+    var numbers = numbers.sorted()
+
+    return max(numbers[0] * numbers[1], numbers[numbers.count-1] * numbers[numbers.count - 2])
+}
+
+solution([10, -20, 30, 5, 5, -20, 5])
+
 ```
 
 ### 정리 <br>
-
+https://peppo.tistory.com/163
 
 문제 출처 <br>
 https://school.programmers.co.kr/learn/courses/30/lessons/120862
