463b2630dd34973d71d2eacf416f9123718116a0
bhoon-coding <bhooncoding@gmail.com>
2022-11-07 23:16:10 +0900

    [프로그래머스] 제일 작은 수 제거하기

---
+++ Programmers/Lv.1/제일 작은 수 제거하기/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+func solution(_ arr:[Int]) -> [Int] {
+
+    let min = arr.sorted(by: <)[0]
+    print(arr.filter { $0 != min })
+    guard arr.count > 1 else { return [-1] }
+    guard let index = arr.firstIndex(of: arr.min()!) else { return [-1] }
+
+    var result: [Int] = arr
+    result.remove(at: index)
+    return result
+}
+
+//func solution(_ arr:[Int]) -> [Int] {
+//    let min = arr.sorted(by: <)[0]
+//    return arr.count == 1 ? [-1] : arr.filter { $0 != min }
+//}
+
+
+//solution([10,3,2,1])
+
+// TODO: [x] 비교할게 없을때 [-1] return
+// TODO: [] 최소값 구하기 min() 메서드
+// TODO: [] 제일 작은값의 인덱스 구하기
+// TODO: [] 구한 인덱스 삭제 remove(at:)
---
+++ Programmers/Lv.1/제일 작은 수 제거하기/README.md
@@ -0,0 +1,23 @@
+#  제일 작은 수 제거하기
+
+### 문제
+<img src="https://user-images.githubusercontent.com/64088377/200204642-7104e654-0261-49cf-87cd-6ff34881158a.png" width="500" >
+
+
+### 풀이 <br>
+```swift
+  func solution(_ arr:[Int]) -> [Int] {
+    guard arr.count > 1 else { return [-1] }
+    guard let index = arr.firstIndex(of: arr.min()!) else { return [-1] }
+    
+    var result: [Int] = arr
+    result.remove(at: index)
+    return result
+}
+```
+
+### 정리 <br>
+https://peppo.tistory.com/169
+
+문제 출처 <br>
+https://school.programmers.co.kr/learn/courses/30/lessons/12935
