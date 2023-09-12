1721a20ec2cf4b0b8df803e4c83c958faf6c9b98
bhoon-coding <bhooncoding@gmail.com>
2023-09-12 09:00:53 +0900

    [LeetCode] Linked List Design 완료

--- LeetCode/Explore/Reverse Linked List/MyPlayground.playground/Contents.swift
+++ LeetCode/Explore/Reverse Linked List/MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+///
 
-var greeting = "Hello, playground"
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        guard let root = head, var rhs = root.next else { return head }
+        let node = reverseList(rhs)
+        rhs.next = root
+        root.next = nil
+        print(node)
+        return node
+        
+    }
+}
+
+Solution().reverseList(ListNode(1, 2))
