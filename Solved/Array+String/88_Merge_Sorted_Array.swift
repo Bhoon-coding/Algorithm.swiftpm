7a35a6af3643437eb438d1e3c7bd711a9b543983
monica <66169740+3dots3craters@users.noreply.github.com>
2024-01-08 07:29:26 +0900

    Add other people's solutions(1)

--- Solved/Array+String/88_Merge_Sorted_Array.swift
+++ Solved/Array+String/88_Merge_Sorted_Array.swift
@@ -21,3 +21,15 @@ class Solution {
         nums1 = (nums1 + tmpNums2).sorted()
     }
 }
+
+
+// other people's solutions
+
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        // while문 써서 돌릴 것 없이 그냥 nums1[..<m] 처럼 범위로 자르면 그만이었던... 이건 문제를 잘못 이해했다.
+        // nums1, nums2 둘 다 마지막이 0인 경우에만 자르는 것이라고 이해했다.
+        // 문제를 꼼꼼히 읽자.
+        nums1 = (nums1[..<m]+nums2).sorted()
+    }
+}
