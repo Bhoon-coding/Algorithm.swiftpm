3077537e78184213ab9f4684c4f67113f2c08a0d
seocho <73249915+whtjdqls617@users.noreply.github.com>
2024-01-08 21:15:52 +0900

    Delete LeetCode directory

--- LeetCode/Explore/Find Numbers with Even Number of Digits/MyPlayground.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-class Solution {
-  func findNumbers(_ numbs: [Int]) -> Int {
-    return numbs.filter { String($0).count % 2 == 0 }.count
-  
-  }
-}
--- LeetCode/Explore/Max Consecutive Ones/MyPlayground.playground/Contents.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-let now = Date()
-let tomorrow = Date(timeIntervalSinceNow: 86400)
