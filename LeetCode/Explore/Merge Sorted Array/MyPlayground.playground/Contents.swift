31cebb022ca715faed3429da599b7c0fdfcf92ae
bhoon-coding <bhooncoding@gmail.com>
2024-01-09 09:40:42 +0900

    [LeetCode] Merge Sorted Array 리팩토링

--- LeetCode/Explore/Merge Sorted Array/MyPlayground.playground/Contents.swift
+++ LeetCode/Explore/Merge Sorted Array/MyPlayground.playground/Contents.swift
@@ -1,26 +1,5 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var newNums1Array: [Int] = []
-        var newNums2Array: [Int] = []
-        
-        newNums1Array = sortedArray(nums1, m)
-        newNums2Array = sortedArray(nums2, n)
-        nums1 = (newNums1Array + newNums2Array).sorted()
-    }
-    
-    func sortedArray(_ nums: [Int], _ n: Int) -> [Int] {
-        var newArray: [Int] = []
-        
-        guard n != 0 else { return newArray }
-        
-        for num in 0...n-1 {
-            newArray.insert(nums[num], at: newArray.endIndex)
-        }
-        
-        return newArray
+       nums1 = (nums1[..<m]+nums2).sorted()
     }
 }
-
-var nums1 = [1,2,3,0,0,0]
-
-Solution().merge(&nums1, 3, [2,5,6], 3)
