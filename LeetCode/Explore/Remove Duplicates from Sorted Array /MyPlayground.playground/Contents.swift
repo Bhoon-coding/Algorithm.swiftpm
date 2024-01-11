08698c69c7613c464eda5340f8019fd03065ca10
bhoon-coding <bhooncoding@gmail.com>
2024-01-11 09:57:11 +0900

    [LeetCode] Remove Duplicates from Sorted Array 1차 제출 (리팩필요)

--- LeetCode/Explore/Remove Duplicates from Sorted Array /MyPlayground.playground/Contents.swift
+++ LeetCode/Explore/Remove Duplicates from Sorted Array /MyPlayground.playground/Contents.swift
@@ -1,9 +1,21 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-     
-        return 0
+        guard nums.isEmpty == false else { return 0 }
+        var resultArr: [Int] = []
+        for num in nums {
+            if resultArr.isEmpty {
+                resultArr.append(num)
+            }
+            if resultArr[resultArr.endIndex - 1] == num {
+                nums.removeFirst()
+            } else {
+                resultArr.append(num)
+            }
+        }
+        nums = resultArr
+        return nums.count
     }
 }
 
-var nums = [1,1,2]
+var nums = [0,0,1,1,1,2,2,3,3,4]
 Solution().removeDuplicates(&nums)
