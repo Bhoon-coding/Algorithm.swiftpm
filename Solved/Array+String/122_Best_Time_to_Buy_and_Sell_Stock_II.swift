aa328335c5ab62fd46e8de4d25a2ddf19f7d992f
monica <66169740+3dots3craters@users.noreply.github.com>
2024-01-24 09:58:21 +0900

    Update 122_Best_Time_to_Buy_and_Sell_Stock_II.swift

--- Solved/Array+String/122_Best_Time_to_Buy_and_Sell_Stock_II.swift
+++ Solved/Array+String/122_Best_Time_to_Buy_and_Sell_Stock_II.swift
@@ -3,12 +3,12 @@ class Solution {
         var maxProfit = 0
 
         var buyPrice = prices[0]
-        for cellPrice in prices {
-            if cellPrice > buyPrice {
-                maxProfit += cellPrice - buyPrice
-                buyPrice = cellPrice
-            } else if cellPrice < buyPrice {
-                buyPrice = cellPrice
+        for sellPrice in prices {
+            if sellPrice > buyPrice {
+                maxProfit += sellPrice - buyPrice
+                buyPrice = sellPrice
+            } else if sellPrice < buyPrice {
+                buyPrice = sellPrice
             }
         }
         return maxProfit
