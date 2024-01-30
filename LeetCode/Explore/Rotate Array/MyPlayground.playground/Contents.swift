9c6d110e5755f246fcc5f4949fa45263606e2cd1
bhoon-coding <bhooncoding@gmail.com>
2024-01-31 00:09:19 +0900

    [LeetCode] Rotate 제출 (복습 필요)

---
+++ LeetCode/Explore/Rotate Array/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        var k = k % nums.count
+        let n = nums.count - 1
+        reverse(&nums, 0, n)
+        reverse(&nums, 0, k - 1)
+        reverse(&nums, k, n)
+    }
+    
+    func reverse(_ nums: inout [Int], _ s: Int, _ e: Int) {
+        var s = s
+        var e = e
+        while s < e {
+            nums.swapAt(s, e)
+            s += 1
+            e -= 1
+        }
+    }
+}
+
+var nums = [-1,-100,3,99, 10]
+Solution().rotate(&nums, 2)
+
+
