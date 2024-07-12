5e443bbc967e158095985b78bf964982f5bcd4e6
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-07-12 16:34:26 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    var result: Int = 0
+    
+    switch angle {
+    case 0..<90:
+        result = 1
+    case 90:
+        result = 2
+    case 90..<180:
+        result = 3
+    case 180:
+        result = 4
+    default:
+        break
+    }
+    
+    return result
+}
