808e358958bbdab4e0063d36956e52dcfe547539
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-07-17 10:04:04 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 6.12 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -1,19 +1,18 @@
 import Foundation
 
-func solution(_ s:String) -> [Int] {
-    var charDict: [Character: Int] = [:]
+func solution(_ s: String) -> [Int] {
+    var word: [Character: Int] = [:]
     var result: [Int] = []
-    
-    s.enumerated().forEach { (index, char) in
-        if let prevCharIndex = charDict[char] {
-            charDict[char] = index
-            result += [index - prevCharIndex]
+
+    for (index, val) in s.enumerated() {
+        if let beforeIndex = word[val] {
+            result.append(index - beforeIndex)
         } else {
             result.append(-1)
-            charDict[char] = index
         }
+
+        word[val] = index
     }
+
     return result
 }
-
-//solution("banana")
