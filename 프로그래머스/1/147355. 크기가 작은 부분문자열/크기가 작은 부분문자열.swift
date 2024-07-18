6df93d86c5256c8351ef646a2da63b11bd5fb072
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-07-18 10:04:53 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 1056.07 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ t:String, _ p:String) -> Int {
+    var tempT: String = t
+    let pLength = p.count
+    var tempArr: [Int] = []
+    
+//    while !tempT.isEmpty && tempT.count != pLength {
+    while tempT.count >= pLength {
+        let element = Int(tempT.prefix(pLength))!
+        tempT.removeFirst()
+        tempArr.append(element)
+    }
+    
+    return tempArr.filter { $0 <= Int(p)! }.count
+}
