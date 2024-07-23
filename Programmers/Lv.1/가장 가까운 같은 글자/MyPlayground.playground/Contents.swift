4719ded9a864f1b3df2198d41dfe9058c963ad52
bhoon-coding <bhooncoding@gmail.com>
2024-07-23 13:51:06 +0900

    [프로그래머스] 복습 - 가장 가까운 같은 글자 풀이

--- Programmers/Lv.1/가장 가까운 같은 글자/MyPlayground.playground/Contents.swift
+++ Programmers/Lv.1/가장 가까운 같은 글자/MyPlayground.playground/Contents.swift
@@ -20,18 +20,72 @@ import Foundation
 
 // 리팩토링
 
+//func solution(_ s: String) -> [Int] {
+//    var word: [Character: Int] = [:]
+//    var result: [Int] = []
+//
+//    for (index, val) in s.enumerated() {
+//        if let beforeIndex = word[val] {
+//            result.append(index - beforeIndex)
+//        } else {
+//            result.append(-1)
+//        }
+//        word[val] = index
+//    }
+//
+//    return result
+//}
+
+
+//func solution(_ s: String) -> [Int] {
+//    var word: [Character: Int] = [:]
+//    var result: [Int] = []
+//    
+//    /// Dictionary 타입의 word 생성
+//    /// word[val] 로 word에 Charactor 값이 있는지 확인
+//    /// 없으면 result에 -1 append
+//    /// 있으면 해당 word 값(index)에 해당 index로 갈아끼우기 or 추가
+//    /// 반복문 한바퀴 돈 다음 char: index 저장
+//    
+//    for (idx, val) in s.enumerated() {
+//        print(word[val])
+//        if let beforeIdx = word[val] {
+//            result.append(idx - beforeIdx)
+//        } else {
+//            result.append(-1)
+//        }
+//        word[val] = idx
+//    }
+//    
+////    print(result)
+//
+////    for (index, val) in s.enumerated() {
+////        if let beforeIndex = word[val] {
+////            result.append(index - beforeIndex)
+////        } else {
+////            result.append(-1)
+////        }
+////        word[val] = index
+////    }
+//
+//    return result
+//}
+
+// TODO: [] 한번더 풀기 (마지막 풀이 7.23)
 func solution(_ s: String) -> [Int] {
     var word: [Character: Int] = [:]
     var result: [Int] = []
-
-    for (index, val) in s.enumerated() {
-        if let beforeIndex = word[val] {
-            result.append(index - beforeIndex)
+    
+    for (index, char) in s.enumerated() {
+        if let beforeIdx = word[char] {
+            result.append(index - beforeIdx)
         } else {
             result.append(-1)
         }
-        word[val] = index
+        word[char] = index
     }
-
+    print("result: \(result)")
     return result
 }
+
+solution("banana")
