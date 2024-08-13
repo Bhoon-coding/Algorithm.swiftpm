a2e7023a455315a7c1efdd4bcef251e344f71079
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-08-13 09:31:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.23 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,44 +1,10 @@
-import Foundation
-
-let numDic: [String: Int] = [
-    "zero": 0,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9
-]
-
 func solution(_ s:String) -> Int {
-
-    var tempArr: String = s
-    var tempStrArr: [String] = []
-    var resultArr: [Int] = []
+    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    var str = s
     
-    // Int인지 확인
-    // TODO: [] Int가 아니면 tempStrArr에 append
-    // TODO: [] Int면 result에 append
-    // numDic 탐색 tempStrArr에
-    
-    for i in tempArr {
-        if let num = Int(String(i)) {
-            resultArr.append(num)
-        } else {
-            tempStrArr.append(String(i))
-        }
-        let joinedStr = tempStrArr.joined()
-        if let convertedNum = numDic[joinedStr] {
-            resultArr.append(convertedNum)
-            tempStrArr.removeAll()
-        }
+    for i in 0..<arr.count {
+        str = str.replacingOccurrences(of: arr[i], with: String(i))
     }
     
-    let toString = resultArr.map { String($0) }.joined()
-    let result = Int(toString) ?? 0
-    
-    return result
+    return Int(str)!
 }
