1ece0ac490ee5c39288a2c7f60573342164a6bc4
bhoon-coding <bhooncoding@gmail.com>
2024-08-13 09:05:29 +0900

    [프로그래머스]기사단원의 무기 다시풀이

--- Programmers/Lv.1/기사단원의 무기/MyPlayground.playground/Contents.swift
+++ Programmers/Lv.1/기사단원의 무기/MyPlayground.playground/Contents.swift
@@ -26,3 +26,29 @@ func solution(_ number: Int, _ limit: Int, _ power: Int) -> Int {
 }
 
 solution(5, 2, 1)
+
+
+// 다시 풀어보기
+
+import Foundation
+
+func solution2(_ number:Int, _ limit:Int, _ power:Int) -> Int {
+    var divisorCount: [Int] = Array(repeating: 0, count: number + 1)
+    var totalIronWeight: Int = 0
+    
+    // 약수 구하기
+    for i in 1...number {
+        for j in stride(from: i, through: number, by: i) {
+            divisorCount[j] += 1
+        }
+    }
+    // 필요한 철의 무게 구하기
+    for currentPower in divisorCount[1...] {
+        totalIronWeight += currentPower > limit ? power : currentPower
+    }
+    
+    return totalIronWeight
+}
+
+solution2(5, 3, 2)
+//solution2(5, 2, 1)
