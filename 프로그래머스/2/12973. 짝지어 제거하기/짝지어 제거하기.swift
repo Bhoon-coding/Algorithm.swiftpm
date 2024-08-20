7f56b80c3449517c42a4439dcd5b3a7b116ed0f4
Byeonghoon Lee <64088377+Bhoon-coding@users.noreply.github.com>
2024-08-20 09:25:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 202.58 ms, Memory: 20.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -0,0 +1,13 @@
+func solution(_ s:String) -> Int {
+    var tempArr: [Character] = []
+    
+    for char in s {
+        if let last = tempArr.last, last == char {
+            tempArr.removeLast()
+        } else {
+            tempArr.append(char)
+        }
+    }
+    
+    return tempArr.isEmpty ? 1 : 0
+}
